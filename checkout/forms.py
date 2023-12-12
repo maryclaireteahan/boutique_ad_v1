41fcc7456444a904d793f8a7ff953ae637aa630e
maryclaireteahan <maryclaireteahan@gmail.com>
2023-12-12 22:39:18 +0000

    feat: add overlay

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,47 +1,39 @@
 from django import forms
 from .models import Order
 
+
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                  'country', 'postcode', 'town_or_city',
                   'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
                   'county',)
-    
+
     def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
-        
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
             'country': 'Country',
-            'postcode': 'Postcode',
+            'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County, State or Locality',
+            'county': 'County',
         }
-        
+
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-                self.fields[field].label = False
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
             else:
-                self.fields[field].widget.attrs['class'] = 'stripe-style-select'
                 placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-                self.fields[field].label = False  # Hide the label for the country field
-                self.fields[field].widget.attrs['class'] = 'stripe-style-input'  # Add the desired class for consistenc
-                
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
