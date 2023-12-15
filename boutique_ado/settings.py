0f04fa979dc510ccdbbfdecc69d181f850c8cde2
maryclaireteahan <maryclaireteahan@gmail.com>
2023-12-15 01:41:12 +0000

    feat: deploy

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
+SECRET_KEY = os.environ.get('SECRET', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ 
 
 ALLOWED_HOSTS = ['maryclaireteahan-boutique-ado.com', 'localhost', 'maryclaireteahan-boutique-ado-f71f271b6fd1.herokuapp.com']
 
