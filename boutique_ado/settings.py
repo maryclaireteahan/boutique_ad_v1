d0e78b632207db58896caaadff241eb747393b15
maryclaireteahan <maryclaireteahan@gmail.com>
2023-12-18 19:24:54 +0000

    fix: settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -219,7 +219,7 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     EMAIL_PORT = 587
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     
